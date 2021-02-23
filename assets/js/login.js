77cd3021070e6e940c4f5aeb74336e6539c77c5e
congwang <congwang225@outlook.com>
2021-02-23 14:34:35 +0800

    登陆页面完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,4 +7,55 @@ $(function () {
         $('.login-box').show();
         $('.reg-box').hide();
     });
+
+    const form = layui.form;
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码不符合规则'],
+        repwd: function (value) {
+            const password = $('.reg-box [name = password]').val();
+            if (password !== value) {
+                return '两次输入不一致';
+            };
+        }
+    });
+
+
+    //监听注册表单的提交行为
+    $('#reg-form').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: '/api/reguser',
+            type: 'POST',
+            data: {
+                username: $('#reg-form [name = username]').val(),
+                password: $('#reg-form [name = password]').val()
+            },
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                };
+                layer.msg('注册成功');
+                $('#link-login').click();
+            }
+        })
+    });
+
+    //监听登陆表单的提交行为
+    $('#login-form').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: '/api/login',
+            type: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                };
+                layer.msg(res.message);
+                sessionStorage.setItem('token', res.token);
+                location.href = './index.html';
+            }
+        });
+    });
+
 })
