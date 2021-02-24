77c618148426e65a6332bbb78821e4a8c911853e
congwang <congwang225@outlook.com>
2021-02-24 16:06:32 +0800

    完成了个人中心功能

--- assets/js/base.js
+++ assets/js/base.js
@@ -9,39 +9,13 @@ $.ajaxPrefilter(function (options) {
     };
 
 
-    options.success = function (res) {
-        console.log(res);
-        const { status, message } = res;
+
+    options.complete = function (response) {
+        console.log(response);
+        const { status, message } = response.responseJSON;
         if (status === 1 && message === "身份认证失败！") {
             localStorage.removeItem('token');
             location.href = './login.html'
-        } else {
-            renderAvatar(res.data);
         }
     }
-    function renderAvatar(user) {
-        const username = user.nickname || user.username;
-        $('#welcome').html('欢迎' + username);
-        //判断是否存在用户头像
-        if (user.user_pic) {
-            //存在用户头像则把图片地址给img的src并显示img
-            $('.layui-nav-img').attr('src', user.user_pic).show();
-            $('.text-avatar').hide();
-        } else {
-            //不存在用户头像则隐藏头像img标签
-            $('.layui-nav-img').hide();
-            //获取用户名首字母并转为大写
-            const firstName = username[0].toUpperCase();
-            //将用户名首字母填入字母头像盒子并显示字母头像盒子
-            $('.text-avatar').html(firstName).show();
-        }
-    }
-    // options.complete = function (response) {
-    //     console.log(response);
-    //     const { status, message } = response.responseJSON;
-    //     if (status === 1 && message === "身份认证失败！") {
-    //         localStorage.removeItem('token');
-    //         location.href = './login.html'
-    //     }
-    // }
 })
---
+++ assets/js/user/user_info.js
@@ -0,0 +1,45 @@
+$(function () {
+    var form = layui.form
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) {
+                return '昵称长度必须在 1 ~ 6 个字符之间！'
+            }
+        }
+    })
+
+    initUserInfo();
+    function initUserInfo() {
+        $.ajax({
+            type: 'GET',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败！')
+                }
+                console.log(res);
+                form.val('userInfo', res.data)
+
+            }
+        })
+    };
+    $('#btnReset').click(function (e) {
+        e.preventDefault();
+        initUserInfo();
+    });
+    $('.layui-form').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function (res) {
+                console.log(res);
+                window.parent.getUserInfo()
+            }
+        })
+
+    })
+
+})
+
